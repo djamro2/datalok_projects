00a1b3b383c7fd18ff55aaadb5c8b4a329bb7b36
Daniel Jamrozik <djamro2@illinois.edu>
2017-02-14 19:02:57 -0600

    create comments structure

--- subreddit_book_recommender/get_amazon_books.py
+++ subreddit_book_recommender/get_amazon_books.py
@@ -1,13 +1,35 @@
 
+from pprint import pprint
 import json
 
-# open the files
-f = open('amazon_comments.txt', 'r', encoding='utf-8')
-output = open('amazon_books.json', 'w', encoding='utf-8')
-comments = f.read().split()
+f = open('out/webdev_comments.json', 'r', encoding='latin-1')
+books_output = open('amazon_books.json', 'w', encoding='latin-1')
+posts = json.loads(f.read())
+books = {}
+comments = []
 
+# build a list of comments to make things easier
+for postId in posts.keys():
+	comments.extend(posts[postId]['comments'])
 print('Number of comments:', len(comments))
 
+for comment in comments:
+	if 'amazon.com' not in comment and 'amazon.co.uk' not in comment:
+		continue
+	for word in comment.split():
+		if '/dp/' in word:
+			s = word.index('/dp/') + 4
+			e = s + 10
+			if word[s:e] not in books.keys():
+				books[word[s:e]] = {}
+				books[word[s:e]]['count'] = 1
+				books[word[s:e]]['comments'] = [comment]
+			else:
+				books[word[s:e]]['count'] += 1
+				books[word[s:e]]['comments'].append(comment)
+
+json.dump(books, books_output)
+
 # close files
-comments.close()
+books_output.close()
 f.close()
